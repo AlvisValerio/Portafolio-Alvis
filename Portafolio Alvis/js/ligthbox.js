314c7f999bf1c43e10b434e28c46fe1b39d50705
AlvisValerio <Alvis_Valerio@Outlook.Com>
2021-05-07 00:46:33 -0400

    Add files via upload

---
+++ Portafolio Alvis/js/ligthbox.js
@@ -0,0 +1,5 @@
+ 
+const imagenes = document.querySelectorAll ('.img-galeria') 
+const imagenesLight = document.querySelector ('.agregar-imagen')
+const contenedorLight = document.querySelector('.imagen-light')
+
